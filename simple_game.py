455efab985e6b7fc04930d0f13ab42c297687438
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-04-06 22:32:50 +0800

    Add files via upload

---
+++ simple_game.py
@@ -0,0 +1,7 @@
+color= input ("Your favorite color: ")
+quotes= input ("Favorite quotes= ")
+celebrity= input ("Favorite celeb= ")
+
+print ("Roses are " + color)
+print ("People once told me "+ quotes)
+print (celebrity + " is my favorite actor")
